0ba78b21430c3a61b32b02150ecc24bc7f50b55e
Velada.eth <96037190+VELADA3@users.noreply.github.com>
2023-05-01 17:37:48 -0400

    gpt reboot 02

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-document.getElementById("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("form").addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-  const prompt = document.getElementById("prompt").value;
-  const conversationHistory = document.getElementById("conversationHistory").value;
-  const resolution = document.getElementById("resolution").value;
-  const format = document.getElementById("format").value;
-  const theme = document.getElementById("theme").value;
+    const prompt = document.getElementById("prompt").value;
+    const conversationHistory = document.getElementById("conversationHistory").value;
+    const resolution = document.getElementById("resolution").value;
+    const format = document.getElementById("format").value;
+    const theme = document.getElementById("theme").value;
 
-  const response = await fetch('http://localhost:5000', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      prompt: conversationHistory + prompt,
-      resolution,
-      format,
-      theme
-    })
+    const response = await fetch('http://localhost:5000', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        prompt: conversationHistory + prompt,
+        resolution,
+        format,
+        theme
+      })
+    });
+
+    if (response.ok) {
+      const { imageURL } = await response.json();
+      const imgElement = document.getElementById("generatedImage");
+      imgElement.src = imageURL;
+    } else {
+      console.error("Failed to generate image");
+    }
   });
-
-  if (response.ok) {
-    const { imageURL } = await response.json();
-    const imgElement = document.getElementById("generatedImage");
-    imgElement.src = imageURL;
-  } else {
-    console.error("Failed to generate image");
-  }
 });
