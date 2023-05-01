8121cdf55b21f34c27199c3fb2bf8026c968a72f
Velada.eth <96037190+VELADA3@users.noreply.github.com>
2023-05-01 17:50:26 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("form").addEventListener("submit", async (e) => {
-    e.preventDefault();
+const form = document.querySelector('form');
+const imageContainer = document.querySelector('#image_container');
 
-    const prompt = document.getElementById("prompt").value;
-    const conversationHistory = document.getElementById("conversationHistory").value;
-    const resolution = document.getElementById("resolution").value;
-    const format = document.getElementById("format").value;
-    const theme = document.getElementById("theme").value;
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
 
-    const response = await fetch('http://localhost:5000', {
+  const formData = new FormData(form);
+  const prompt = formData.get('prompt');
+
+  try {
+    const response = await fetch('http://localhost:5000/generate', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        prompt: conversationHistory + prompt,
-        resolution,
-        format,
-        theme
-      })
+        prompt,
+      }),
     });
 
     if (response.ok) {
-      const { imageURL } = await response.json();
-      const imgElement = document.getElementById("generatedImage");
-      imgElement.src = imageURL;
+      const data = await response.json();
+      const imageUrl = data.data[0].url;
+      imageContainer.innerHTML = `<img src="${imageUrl}" alt="Generated Image">`;
     } else {
-      console.error("Failed to generate image");
+      const err = await response.text();
+      imageContainer.innerHTML = '<p>Something went wrong</p>';
+      console.error(err);
     }
-  });
+  } catch (error) {
+    console.error(error);
+  }
 });
